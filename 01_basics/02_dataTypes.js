35125f6152b080828f1c6d7e39ef60ca64006664
Nagesh <yashchavan192001@gmail.com>
2024-05-26 21:39:14 +0530

    basics of js variables and data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Hello")
+
+
+let name = "Nagesh";
+let age = 21;
+let isLoggedIn = false;
+let a = null;
+let state;
+
+// number => 2 to power 53 // range of number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value // for eg. let a = null // value of a is empty  
+// undefined => a value is not define yet to the variable // for eg. let state;
+// symbol => unique // it mosty used for uniqueness // for eg. in react
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
