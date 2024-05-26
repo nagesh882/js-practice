3d3370f89c57327a26a192c56525a3bf9ff8e385
Nagesh <yashchavan192001@gmail.com>
2024-05-27 00:13:14 +0530

    Operations added in this file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,51 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33 // value => 33 // typeof => string
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ************************* Operations *******************************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " nagesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // vlaue => 122
+// console.log(1 + 2 + "2"); // vlaue => 32 // Not prefer in production avoid this
+
+// console.log(true); // value => true // Not prefer in production avoid this
+// console.log(+true); // vlaue => 1 // Not prefer in production avoid this
+// console.log(+""); // vlaue => 0 // Not prefer in production avoid this
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2 
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
