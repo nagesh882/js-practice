fbcf72345f1910aa44b0ab414c526001c662c8b4
Nagesh <yashchavan192001@gmail.com>
2024-05-27 19:38:46 +0530

    practice done of all string methods

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -30,11 +30,61 @@ const newStringOne = "     Nagesh        "
 
 const url = "http://nagesh.com/nagesh%20chavan"
 
-console.log(url.replace('%20', '-'));
+// console.log(url.replace('%20', '-'));
 
 
-console.log(url.includes('nagesh'))
-console.log(url.includes('raj'))
+// console.log(url.includes('nagesh'))
+// console.log(url.includes('raj'))
 
 
-console.log(gameName.split('-'))
+// console.log(gameName.split('-'))
+
+
+
+let myString = "character"
+
+// console.log(myString.length)
+
+// console.log(myString.charAt(6))
+
+// console.log(myString.charCodeAt(6))
+
+// console.log(myString.concat(" What?"))
+
+// console.log(myString.endsWith("r"))
+
+// console.log(myString.includes('ract'))
+
+// console.log(myString.indexOf('t'))
+
+// console.log(myString.lastIndexOf("a"))
+
+// console.log(myString.lastIndexOf(-1))
+
+// console.log(myString.repeat(2))
+
+// console.log(myString.replace("r", "w"))
+
+// console.log(myString.replaceAll("r", "w"))
+
+// console.log(myString.slice(0, 8))
+
+const urlTwo = "https://www.msn.com/en-in/sports/cricket/not-just-virat-kohli-2-more-indian-players-to-leave-for-t20-world-cup-at-a-later-date-report/ar-BB1n4w3m?ocid=msedgdhp&pc=U531&cvid=a3adcd812e6540c18fe3c7da7e13a99d&ei=10"
+
+// console.log(urlTwo.split("/"))
+
+// console.log(myString.startsWith("c"))
+
+// console.log(myString.substring(1, 5))
+
+// console.log(myString.toLowerCase())
+
+// console.log(myString.toUpperCase())
+
+const str = "   Hello     "
+
+console.log(str.trim())
+
+console.log(str.trimEnd())
+
+console.log(str.trimStart())
