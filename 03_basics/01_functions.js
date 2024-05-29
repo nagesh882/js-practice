98bb62cf9cf68e6de1bf64ea9b57b944c78c7769
Nagesh <yashchavan192001@gmail.com>
2024-05-30 00:24:55 +0530

    functions part2 in javascript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -18,6 +18,8 @@ function sayMyName(){
 
 // };
 
+// Parameters are the variables listed inside the parentheses in the function definition. They act as placeholders for the values that will be passed to the function when it is called.
+
 function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
@@ -29,6 +31,8 @@ function addTwoNumbers(number1, number2){
 };
 
 const result = addTwoNumbers(3, 5)
+
+// Arguments are the actual values that are passed to the function when it is called.
 
 // console.log("Result:", result)
 
@@ -45,3 +49,42 @@ function loginUserMessage(username = "sam"){
 
 // console.log(loginUserMessage("nagesh"))
 // console.log(loginUserMessage("Yash"))
+
+
+
+// (...num1) // rest operator
+// function calculateCartPrice(...num1){
+//     return num1
+// };
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+};
+
+// console.log(calculateCartPrice(200,400,500,600,5000))
+
+
+const user = {
+    username: "yash",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+};
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 500, 1000]))
