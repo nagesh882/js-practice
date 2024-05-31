37896145cce6a846ac9dfee7549c1a9f9e3423b5
Nagesh <yashchavan192001@gmail.com>
2024-05-31 21:43:23 +0530

    constrol flow switch case

---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// const month = 5
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
