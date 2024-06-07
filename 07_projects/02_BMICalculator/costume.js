4fef1cd701ff70e5428a98d402635116b7f77523
Nagesh <yashchavan192001@gmail.com>
2024-06-07 22:57:06 +0530

    BMI calculator project done

---
+++ 07_projects/02_BMICalculator/costume.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+// this use case give you empty vlaue
+// const height = parseInt(document.querySelector("#height").value)
+
+form.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector("#height").value);
+    
+    const weight = parseInt(document.querySelector("#weight").value);
+
+    const results = document.querySelector("#results");
+
+    if ( height === "" || height < 0 || isNaN(height) ) {
+        const result = document.createElement("span");
+        result.appendChild(document.createTextNode(`Please give a valid height ${height}`));
+        results.replaceChildren(result);
+
+    } else if ( weight === "" || weight < 0 || isNaN(weight) ) {
+        results.innerHTML = `Please give a valid weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height * height)/10000)).toFixed(2)
+
+        // show the result
+
+        results.innerHTML = `<span>${bmi}</span>`
+    }
+
+})
